8d97014a1e6d099d93a76abd8fc760479b2bbdbb
继风 <jifeng.zjd@taobao.com>
2016-05-31 18:19:04 +0800

    first commit

---
+++ src/com/jifeng/Basic.java
@@ -0,0 +1,14 @@
+package com.jifeng;
+import com.jifeng.Person;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Person person = new Person("jifeng");
+		person.changeName("zhujiadun");
+		person.printName();
+	}
+
+}
---
+++ src/com/jifeng/Person.java
@@ -0,0 +1,19 @@
+package com.jifeng;
+
+public class Person {
+	String name;
+	int age;
+	public Person (String name) {
+		System.out.println("Person is " + name);
+	}
+
+	public void changeName (String name) {
+		this.name = name;
+		return;
+	}
+	
+	public String printName () {
+		System.out.println("Person is " + name);
+		return this.name;
+	}
+}
